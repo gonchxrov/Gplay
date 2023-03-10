51e3cceb7bdec0985e816f4815454469c7759d13
Oleksii Honcharov <oleksii@lymia.eu>
2023-03-10 22:44:57 +0200

    remove unused based file from frontend

--- frontend/src/features/counter/Counter.js
+++ frontend/src/features/counter/Counter.js
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import { decrement, increment, incrementByAmount, incrementAsync, selectCount } from './counterSlice';
 
 export function Counter() {
   const count = useSelector(selectCount);
@@ -16,42 +9,35 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div>
         <button
-          className={styles.button}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span>{count}</span>
         <button
-          className={styles.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
-      <div className={styles.row}>
+      <div>
         <input
-          className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={e => setIncrementAmount(e.target.value)}
         />
-        <button
-          className={styles.button}
+        <button 
           onClick={() =>
             dispatch(incrementByAmount(Number(incrementAmount) || 0))
           }
         >
           Add Amount
         </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
+        <button onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}>
           Add Async
         </button>
       </div>
