83f700bb6e4a277f014035084b43b6609841a600
Oleksii Honcharov <oleksii@lymia.eu>
2023-03-30 02:35:51 +0300

    add create purchase api

--- frontend/src/http/PurchaseAPI.js
+++ frontend/src/http/PurchaseAPI.js
@@ -1,5 +1,10 @@
 import { $hostAuth } from "./index";
 
+export const createPurchase = async (purchase) => {
+  const { data } = await $hostAuth.post("api/purchase", purchase);
+  return data;
+};
+
 export const fetchPurchases = async (userId) => {
   const { data } = await $hostAuth.get(`api/purchase?userId=${userId}`);
   return data;
